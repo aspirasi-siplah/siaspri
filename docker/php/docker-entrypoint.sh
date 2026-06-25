b3eb46f0fbf67257a1e1c99d98ad75a47d269fbf
bagusrizkita <bagusrizkitaitats@gmail.com>
2026-06-25 19:40:05 +0700

    fix(docker/php): add chown for storage and cache
    ensure web user has write access to laravel's required directories

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+
 if [ "${APP_ENV}" = "production" ] || [ "${APP_ENV}" = "staging" ]; then
     echo "Caching config, routes, and views..."
     php artisan config:cache
