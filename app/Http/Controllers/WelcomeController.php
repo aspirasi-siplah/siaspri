12ef599e414a574cd4819d592819263a6e99ccaa
novalp30 <76994059+novalp30@users.noreply.github.com>
2026-07-06 14:31:05 +0700

    bug berita masih muncul meskipun status bukan publish

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,7 +11,7 @@ class WelcomeController extends Controller
     public function index()
     {
         return Inertia::render('welcome', [
-            'latestNews' => News::latest()->limit(3)->get(),
+            'latestNews' => News::where('status', News::STATUS_PUBLISHED)->latest()->limit(3)->get(),
             'stats' => [
                 'totalNews' => News::where('status', News::STATUS_PUBLISHED)->count(),
                 'totalBlacklistMerchant' => BlacklistMerchant::where('status', BlacklistMerchant::STATUS_ACTIVE)->count(),
