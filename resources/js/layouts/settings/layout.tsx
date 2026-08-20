7f2d21b73b72b577b133ba7798d31f35dcb1df16
novalp30 <76994059+novalp30@users.noreply.github.com>
2026-08-20 11:39:44 +0700

    Modul Ubah Password Admin Fixes #31

--- resources/js/layouts/settings/layout.tsx
+++ resources/js/layouts/settings/layout.tsx
@@ -16,11 +16,11 @@ const sidebarNavItems: NavItem[] = [
         href: edit(),
         icon: null,
     },
-    // {
-    //     title: 'Security',
-    //     href: editSecurity(),
-    //     icon: null,
-    // },
+    {
+        title: 'Keamanan',
+        href: editSecurity(),
+        icon: null,
+    },
     {
         title: 'Tampilan',
         href: editAppearance(),
